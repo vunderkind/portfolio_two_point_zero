36a09f59dd50b853f4bb0c55c871bd0c80d0eafc
mogwai <feelirabs@gmail.com>
2020-04-10 00:30:48 +0100

    Added rrd

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -13,21 +13,18 @@ const projects = [
         description: "Reimagined, then designed and built (with a team) a shopping cart experience designed for people with minimal tech-savviness. Incorporated new ideas such as USSD payments, haggling, mobile-first inventory management.",
         tools: "Figma, React, MongoDB.",
         link: "https://pure-retail.com",
-        github: "https://something"
         },
         {
             title: "Race Against Corona",
             description: "A MUD (multi-user dungeon) game with the simple objective of finding a cure for the coronavirus. You start out as the lone healthy person in a city overtaken by COVID-19. You must avoid contamination while seeking out a lab where a cure for the virus is located.",
             tools: "Python (Django), React.",
             link: "https://gamer.vunderkind.now.sh/",
-            github: "https://something"
             },
             {
                 title: "Apple AirPods Cool Scroll Demo",
                 description: "Cool JavaScript scroll effect inspired by Apple's AirPods Pro website.",
                 tools: "Vanilla JavaScript and ScrollMagic.",
                 link: "https://vunderkind.github.io/scrollmagic-demo/",
-                github: "https://something"
                 },
                 {
                     title: "The Motherkind",
@@ -41,7 +38,6 @@ const projects = [
                     description: "A nifty solution to the problem of too many open student tickets, and too few instructors/administrators to handle them.",
                     tools: "React, Redux.",
                     link: "https://frontend.martintmensah.now.sh/",
-                    github: "https://something"
                     },
                     {
                         title: "Piano Reeves",
