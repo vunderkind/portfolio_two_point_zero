d62baa47b44045d5d431cfec295dd1d3d6b30945
mogwai <feelirabs@gmail.com>
2020-04-10 23:30:19 +0100

    Adding Twitter card

--- portfolio/src/Components/Arrow.js
+++ portfolio/src/Components/Arrow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const Arrow = () => {
     return (
-    <img src="https://image.flaticon.com/icons/svg/2026/2026915.svg" alt="arrow" width="10" height="10"/>
+    <img src="https://image.flaticon.com/icons/svg/2223/2223606.svg" alt="arrow" width="10" height="10"/>
     )
   }
 
