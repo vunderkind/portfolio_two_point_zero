1769ea13aba289a00726ce5bacc73f897bda7801
mogwai <feelirabs@gmail.com>
2020-04-12 03:21:34 +0100

    Add about page shell

---
+++ portfolio/src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div className="Home">
+        <div className="Bio">
+          <div className="Move">
+        <h1 className="Hedgehog">{props.theme === 'light'? '🦔':'🦊'}</h1>
+          </div>
+        <h1>{props.theme === 'light'? 'Hello! I am Justin Irabor.': 'mogwai'}</h1>
+        {props.theme === 'light' ? <h2>Full-stack developer. I work to mitigate the balkanization of digital disciplines.<br/> <br/><a href="mailto:'thevunderkind@gmail.com">Let's talk 🤺.</a></h2>: <h2>A problem-solvin' language-learnin' sunuvabish 🙊. Has the mind 🧠 of an artist 🎨 and the keen eye 👀 of a growth engineer 📈.<br/><br/><a href="mailto:'thevunderkind@gmail.com">Hit me! 🏌️.</a></h2>}
+        </div>
+        </div>
+    )
+}
+
+export default Home
